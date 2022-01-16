2bd5a67bc8a43559158299770588a025e9c96443
Braden Bax <bradenbax@gmail.com>
2022-01-15 18:11:47 -0600

    6

--- payCalculator.py
+++ payCalculator.py
@@ -4,7 +4,8 @@ def calculatePay():
     # This first line is provided for you
     hrs = float(input("Enter Hours:"))
     rate = float(input("Enter Rate:"))
-    print(hrs * rate)
+    pay = rate * hrs
+    print("Pay:", pay)
     # end assignment
 
 ## if you want to test locally before you try to sync
