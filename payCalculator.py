b71a619a93cdc91580bbb45ed1ee55c2cb1a4f9f
Braden Bax <bradenbax@gmail.com>
2022-01-15 18:07:43 -0600

    5

--- payCalculator.py
+++ payCalculator.py
@@ -4,8 +4,8 @@ def calculatePay():
     # This first line is provided for you
     hrs = input("Enter Hours:")
     rate = input("Enter Rate:")
-    hrs = int(hrs)
-    rate = int(rate)
+    hrs = float(hrs)
+    rate = float(rate)
     print(hrs * rate)
     # end assignment
 
@@ -13,4 +13,4 @@ def calculatePay():
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-#calculatePay()
+calculatePay()
